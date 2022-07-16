ee4ff08d53a05798f76ca1bb32c6b2ff7565d88f
iMGMoosic <93943831+iMGMoosic@users.noreply.github.com>
2022-07-16 04:53:04 -0500

    Update time.sh

--- rpi-rgb-led-matrix/examples-api-use/time.sh
+++ rpi-rgb-led-matrix/examples-api-use/time.sh
@@ -3,6 +3,6 @@ killall -9 led-image-viewer
 killall -9 video-viewer
 killall -9 text-example
 #cd /home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use
-(while :; do date +%T ; sleep 0.2 ; done) | /home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use/text-example -f /home/pi/led-matrix-controller/rpi-rgb-led-matrix/fonts/9x18B.bdf -C 128,0,0 --led-rows=16 --led-chain=3 >/dev/null
+(while :; do date +%T ; sleep 0.2 ; done) | /home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use/text-example -f /home/pi/led-matrix-controller/rpi-rgb-led-matrix/fonts/9x18B.bdf -C 128,0,0 --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50 >/dev/null
 exit 0
 
