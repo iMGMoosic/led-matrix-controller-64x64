525cdda4233c28ee139ea45c161a9dc5a3a06de2
iMGMoosic <93943831+iMGMoosic@users.noreply.github.com>
2022-07-16 04:56:21 -0500

    Update guinnesspints.sh

--- scripts/guinnesspints.sh
+++ scripts/guinnesspints.sh
@@ -2,5 +2,5 @@ killall -9 demo
 killall -9 led-image-viewer
 killall -9 text-example
 cd /home/pi/led-matrix-controller/rpi-rgb-led-matrix/utils/
-sudo /home/pi/led-matrix-controller/rpi-rgb-led-matrix/utils/led-image-viewer /home/pi/led-matrix-controller/images/guinnesspints.gif --led-rows=16 --led-chain=4 >/dev/null
+sudo /home/pi/led-matrix-controller/rpi-rgb-led-matrix/utils/led-image-viewer /home/pi/led-matrix-controller/images/guinnesspints.gif --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50 >/dev/null
 exit 0
