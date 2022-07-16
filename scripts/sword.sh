7eef1472d0c3d8565079fc723e55d50d98689320
iMGMoosic <93943831+iMGMoosic@users.noreply.github.com>
2022-07-16 04:58:15 -0500

    Update sword.sh

--- scripts/sword.sh
+++ scripts/sword.sh
@@ -2,5 +2,5 @@ killall -9 demo
 killall -9 led-image-viewer
 killall -9 text-example
 cd /home/pi/rpi-rgb-led-matrix/utils/
-sudo /home/pi/rpi-rgb-led-matrix/utils/led-image-viewer sword_pong.gif --led-rows=16 --led-chain=2 >/dev/null
+sudo /home/pi/rpi-rgb-led-matrix/utils/led-image-viewer sword_pong.gif --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50 >/dev/null
 exit 0
