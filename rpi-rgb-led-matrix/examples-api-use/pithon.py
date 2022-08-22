90a23b470d392f2e4bfa455e1e60c055f37a5fbf
iMGMoosic <93943831+iMGMoosic@users.noreply.github.com>
2022-08-22 16:10:21 -0500

    Update pithon.py

--- rpi-rgb-led-matrix/examples-api-use/pithon.py
+++ rpi-rgb-led-matrix/examples-api-use/pithon.py
@@ -48,4 +48,4 @@ for text_color_pair in text:
  
 im.save("/home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use/test.ppm")
  
-os.system("/home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use/demo -D 1 test.ppm --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50 -R 180")
+os.system("/home/pi/led-matrix-controller/rpi-rgb-led-matrix/examples-api-use/demo -D 1 test.ppm --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50")
