7727a1ead9ba762fad29e85fafce1a120953349a
iMGMoosic <93943831+iMGMoosic@users.noreply.github.com>
2022-08-22 16:42:37 -0500

    Update finger.sh

--- scripts/finger.sh
+++ scripts/finger.sh
@@ -1,5 +1,5 @@
 killall -9 demo
 killall -9 led-image-viewer
 killall -9 text-example
-sudo /home/pi/led-matrix-controller/rpi-rgb-led-matrix/utils/led-image-viewer /home/pi/led-matrix-controller/images/fingers.gif --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-gpio-slowdown=4 --led-brightness=50 -R 180 >/dev/null
+sudo /home/pi/led-matrix-controller/rpi-rgb-led-matrix/utils/led-image-viewer /home/pi/led-matrix-controller/images/fingers.gif --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-slowdown-gpio=4 --led-brightness=50 >/dev/null
 exit 0
